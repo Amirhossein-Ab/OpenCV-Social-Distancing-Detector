790154f43b0dd4d27edf878fd17712ba8d6a175a
Amirhossein <amirhosseinarbab@gmail.com>
2020-06-05 13:22:10 +0430

    social distance detector initiated

---
+++ social_distance_detector.py
@@ -0,0 +1,85 @@
+from configuration import social_distancing_config as config
+from configuration.detection import detect_people
+from scipy.spatial import distance as dist
+import numpy as np
+import argparse
+import imutils
+import cv2
+import os
+
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--input", type=str, default="",
+                help="path to (optional) input video file")
+ap.add_argument("-o", "--output", type=str, default="",
+                help="path to (optional) output video file")
+ap.add_argument("-d", "--display", type=int, default=1,
+                help="whether or not output frame should be displayed")
+args = vars(ap.parse_args())
+
+# load the COCO class labels our YOLO model was trained on
+labelsPath = os.path.sep.join([config.MODEL_PATH, "coco.names"])
+LABELS = open(labelsPath).read().strip().split("\n")
+
+# derive the paths to the YOLO weights and model configuration
+weightsPath = os.path.sep.join([config.MODEL_PATH, "yolov3.weights"])
+configPath = os.path.sep.join([config.MODEL_PATH, "yolov3.cfg"])
+
+# load our YOLO object detector trained on COCO dataset (80 classes)
+print("[INFO] loading YOLO from disk...")
+net = cv2.dnn.readNetFromDarknet(configPath, weightsPath)
+
+# check if we are going to use GPU
+if config.USE_GPU:
+    # set CUDA as the preferable backend and target
+    print("[INFO] setting preferable backend and target to CUDA...")
+    net.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
+    net.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA)
+
+# determine only the *output* layer names that we need from YOLO
+ln = net.getLayerNames()
+ln = [ln[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+
+# initialize the video stream and pointer to output video file
+print("[INFO] accessing video stream...")
+vs = cv2.VideoCapture(args["input"] if args["input"] else 0)
+writer = None
+
+# loop over the frames from the video stream
+while True:
+    # read the next frame from the file
+    (grabbed, frame) = vs.read()
+
+    # if the frame was not grabbed, then we have reached the end
+    # of the stream
+    if not grabbed:
+        break
+
+    # resize the frame and then detect people (and only people) in it
+    frame = imutils.resize(frame, width=700)
+    results = detect_people(frame, net, ln,
+                            person_index=LABELS.index("person"))
+
+    # initialize the set of indexes that violate the minimum social
+    # distance
+    violate = set()
+
+    # ensure there are *at least* two people detections (required in
+    # order to compute our pairwise distance maps)
+    if len(results) >= 2:
+        # extract all centroids from the results and compute the
+        # Euclidean distances between all pairs of the centroids
+        centroids = np.array([r[2] for r in results])
+        D = dist.cdist(centroids, centroids, metric="euclidean")
+
+        # loop over the upper triangular of the distance matrix
+        for i in range(0, D.shape[0]):
+            for j in range(i + 1, D.shape[1]):
+                # check to see if the distance between any two
+                # centroid pairs is less than the configured number
+                # of pixels
+                if D[i, j] < config.MIN_DISTANCE:
+                    # update our violation set with the indexes of
+                    # the centroid pairs
+                    violate.add(i)
+                    violate.add(j)
